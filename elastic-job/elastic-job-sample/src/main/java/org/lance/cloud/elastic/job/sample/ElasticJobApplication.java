e120361e8f16e3b17657bf7d5d89cf153b7ded07
Lance <lance.yu.99@gmail.com>
2019-05-12 20:03:13 +0800

    增加Elastic-Job和XXL-Job模块

---
+++ elastic-job/elastic-job-sample/src/main/java/org/lance/cloud/elastic/job/sample/ElasticJobApplication.java
@@ -0,0 +1,12 @@
+package org.lance.cloud.elastic.job.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobApplication.class, args);
+    }
+}
---
+++ xxl-job/xxl-job-sample/src/main/java/org/lance/cloud/xxl/job/sample/XxlJobApplication.java
@@ -0,0 +1,15 @@
+package org.lance.cloud.xxl.job.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * xxl-job 分布式定时任务样例
+ */
+@SpringBootApplication
+public class XxlJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobApplication.class, args);
+    }
+}
